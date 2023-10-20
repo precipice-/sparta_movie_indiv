9b0a3604c3a2ec23551208c2d1d52fe0a72fcc45
precipice- <configure62@gmail.com>
2023-10-20 20:39:08 +0900

    added search function

--- main.js
+++ main.js
@@ -1,69 +1,66 @@
 const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTMzMDJiMTJmZmJkMjMyNzI2M2Y3NzgwN2NkYWU3NiIsInN1YiI6IjY1MmYyOTc5MGNiMzM1MTZmZDRiNzM5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.shEys2KpaKJhQyQVxL97hmAmRPZ101n0SmCtfdFL9dE",
-  },
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTMzMDJiMTJmZmJkMjMyNzI2M2Y3NzgwN2NkYWU3NiIsInN1YiI6IjY1MmYyOTc5MGNiMzM1MTZmZDRiNzM5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.shEys2KpaKJhQyQVxL97hmAmRPZ101n0SmCtfdFL9dE",
+    },
 };
 
 fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
 )
-  .then((response) => response.json())
-  .then((response) => {
-    // console.log(response);
+    .then((response) => response.json())
+    .then((response) => {
+        movies = response.results;
+        drawMovieCard(movies);
 
-    movies = response.results;
-    // console.log(movies);
-    drawMovieCard(movies);
-
-    let cards = document.getElementsByClassName("movie");
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].addEventListener("click", click);
-    }
-  })
-  .catch((err) => console.error(err));
+        let cards = document.getElementsByClassName("movie");
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].addEventListener("click", onClickMovieCard);
+        }
+    })
+    .catch((err) => console.error(err));
 
 let movies;
-let searchInput;
+let searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 searchBtn.addEventListener("click", () => {
-        searchInput = document.querySelector("#search-input");
-        console.log(searchInput);
-        findMovie(movies, searchInput.value);
-    });
+    console.log(searchInput.value);
+    findMovie(movies, searchInput.value);
+});
 
-
-function click(e) {
-  window.alert("id: " + this.id);
+function onClickMovieCard(e) {
+    window.alert("id: " + this.id);
 }
 
+const cardWrapDiv = document.getElementById("card-wrap");
 const drawMovieCard = (collectedMovies) => {
-  document.querySelector(".movie").remove();
-  collectedMovies.forEach((movie) => {
-    let template = `<div class="movie" id="${movie.id}">
+    let childrenLength = cardWrapDiv.children.length;
+    for (let i = 0; i < childrenLength; i++) {
+        cardWrapDiv.children[0].remove();
+    }
+    collectedMovies.forEach((movie) => {
+        let template = `<div class="movie" id="${movie.id}">
                       <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="" />
                       <h2 class="movieName">${movie.title}</h2>
                       <p class="overview">${movie.overview}</p>
                       <p class="movieRate">Rating: ${movie.vote_average}</p>                      
                     </div>`;
 
-    document
-      .querySelector("#card-wrap")
-      .insertAdjacentHTML("beforeend", template);
-  });
+        cardWrapDiv.insertAdjacentHTML("beforeend", template);
+    });
 };
 
 // 영화 객체, 검색참 인풋 / 출력값 받아서 다시 그리기
 const findMovie = (object, keyword) => {
-  let searchResult = [];
+    let searchResult = [];
 
-  for (let x = 0; x < object.length; x++) {
-    if (object[x].title.includes(keyword)) {
-      searchResult.push(object[x]);
+    for (let x = 0; x < object.length; x++) {
+        if (object[x].title.includes(keyword)) {
+            searchResult.push(object[x]);
+        }
     }
-  }
-  drawMovieCard(searchResult);
+    drawMovieCard(searchResult);
 };
